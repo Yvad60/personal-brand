e8d680b940753b9ee16419499e2f09f83af97fad
ivad <yvesyvad@gmail.com>
2022-05-04 00:31:01 +0200

    setup eslint prettier and husky

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # personal-brand
-a refinement of the personal brand built in ATLP core concept rebuild using modern technologies 
+
+a refinement of the personal brand built in ATLP core concept rebuild using modern technologies
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,44 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-module.exports = {
-  entry: 'index.js',
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js',
-  },
-  target: 'web',
-  devServer: {
-    port: '5000',
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    open: true,
-    hot: true,
-    liveReload: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
-    }),
-  ],
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const webpackConfig = {
+	entry: 'index.js',
+	mode: 'development',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'index_bundle.js',
+	},
+	target: 'web',
+	devServer: {
+		port: '5000',
+		static: {
+			directory: path.join(__dirname, 'public'),
+		},
+		open: true,
+		hot: true,
+		liveReload: true,
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.json'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'public', 'index.html'),
+		}),
+	],
 };
+
+export default webpackConfig;
