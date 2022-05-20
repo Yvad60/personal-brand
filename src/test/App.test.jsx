e23cf81d014acbfe4007c2f3de1c6184a4c4abbc
ivad <yvesyvad@gmail.com>
2022-05-20 22:23:56 +0200

    ch(setup): setup redux

--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -1,15 +1,46 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import React from 'react';
 import App from '../App.jsx';
+import store from '../redux/store.js';
 
 describe('App tests', () => {
-	test('should render app without crasing', () => {
-		const wrapper = render(<App />);
+	test('should render app without crashing', () => {
+		const wrapper = render(
+			<Provider store={store}>
+				<App />
+			</Provider>
+		);
 		expect(wrapper).toMatchSnapshot();
 	});
-	it('should contain text', () => {
-		render(<App />);
-		const headingText = screen.getByText(/ivad code/i);
-		expect(headingText).toBeInTheDocument();
+
+	describe('components tests', () => {
+		beforeEach(() => {
+			render(
+				<Provider store={store}>
+					<App />
+				</Provider>
+			);
+		});
+		it('should contain app heading', () => {
+			const headingText = screen.getByText(/Sample counter app/i);
+			expect(headingText).toBeInTheDocument();
+		});
+		it('should display count of 0 by default', () => {
+			const count = screen.getByText(/0/i);
+			expect(count).toBeInTheDocument();
+		});
+		it('should increment on increment button click', () => {
+			const incrementBtn = screen.getByText(/increment/i);
+			fireEvent.click(incrementBtn);
+			const count = screen.getByText(/1/i);
+			expect(count).toBeInTheDocument();
+		});
+		it('should decrement on decrement button click', () => {
+			const decrementBtn = screen.getByText(/decrement/i);
+			fireEvent.click(decrementBtn);
+			const count = screen.getByText(/0/);
+			expect(count).toBeInTheDocument();
+		});
 	});
 });
